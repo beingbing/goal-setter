68272e6402ad98170b086c9ea5a8cf4a1238ac94
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-12 12:45:38 +0530

    form validation, error styling and clear input after sumbit added

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [inputIsValid, setInputIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setInputIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setInputIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !inputIsValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{borderColor: !inputIsValid ? 'red' : 'black', background: !inputIsValid ? 'salmon' : 'transparent'}} value={enteredValue} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
